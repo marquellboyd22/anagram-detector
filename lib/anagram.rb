60de064e02504da991096b54f73d8f95a98e4622
Marquell boyd <marquellboyd@icloud.com>
2020-10-15 20:25:43 -0400

    Update anagram.rb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :array
+    
+    def initialize  (array)
+        @array = array
+    end
+
+    def match(word_array)
+        word_array.select do |array| 
+            word.split("").soft == @word.split("")
+        end
+    end
+end
