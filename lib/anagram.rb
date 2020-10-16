93a03c22c0b31e83eb971038bc846c3d5e163b25
Marquell boyd <marquellboyd@icloud.com>
2020-10-15 20:31:57 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,13 +2,13 @@
 class Anagram
     attr_accessor :array
     
-    def initialize  (array)
-        @array = array
+    def initialize  (name)
+        @array = name
     end
 
     def match(word_array)
         word_array.select do |array| 
-            word.split("").soft == @word.split("")
+            array.split("").sort == @array.split("").sort
         end
     end
 end
